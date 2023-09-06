61e3e45f32fa8319becd53d6d5b8d78a08f55561
natanaelvich <taelima1997@gmail.com>
2023-09-05 22:44:19 -0300

    fix eslint error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,18 @@
-import ButtonLinkedin from "./components/ButtonLinkedin";
+import ButtonLinkedin from './components/ButtonLinkedin'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-       <h1 className="text-4xl font-bold text-center text-gray-900">
-        Generate Portfolio Linkedin
-       </h1>
+        <h1 className="text-4xl font-bold text-center text-gray-900">
+          Generate Portfolio Linkedin
+        </h1>
 
-       <p className="text-center text-gray-700">
-        A simple tool to generate a portfolio from your Linkedin
-       </p>
+        <p className="text-center text-gray-700">
+          A simple tool to generate a portfolio from your Linkedin
+        </p>
 
-       <ButtonLinkedin />
+        <ButtonLinkedin />
       </div>
     </main>
   )
