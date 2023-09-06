1f8b3559e9dc11653571e0466e0d4988572766da
natanaelvich <taelima1997@gmail.com>
2023-09-05 22:41:53 -0300

    add button linkedin

---
+++ src/app/components/ButtonLinkedin.tsx
@@ -0,0 +1,7 @@
+export default function ButtonLinkedin() {
+  return (
+    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      Entrar com Linkedin
+    </button>
+  );
+}
