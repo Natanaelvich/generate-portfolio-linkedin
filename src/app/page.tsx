249150584fe49ee1ff201b25f2abc43baa97d966
natanaelvich <taelima1997@gmail.com>
2023-09-06 22:15:01 -0300

    update styles home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,16 @@ import ButtonLinkedin from './components/ButtonLinkedin'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <h1 className="text-4xl font-bold text-center text-gray-900">
-          Generate Portfolio Linkedin
-        </h1>
+    <main className="flex min-h-screen flex-col items-center justify-center gap-6 p-24">
+      <h1 className="text-4xl font-bold text-center text-gray-900">
+        Generate Portfolio Linkedin
+      </h1>
 
-        <p className="text-center text-gray-700">
-          A simple tool to generate a portfolio from your Linkedin
-        </p>
+      <p className="text-center text-gray-700">
+        A simple tool to generate a portfolio from your Linkedin
+      </p>
 
+      <div className="flex items-center justify-center mt-4">
         <ButtonLinkedin />
       </div>
     </main>
