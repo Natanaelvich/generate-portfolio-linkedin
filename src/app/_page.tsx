cdbdd804c3d4f24ffd3f04441be27ff8dd37a6ad
natanaelvich <taelima1997@gmail.com>
2023-09-06 22:30:20 -0300

    add _app to add session provider

---
+++ src/app/_page.tsx
@@ -0,0 +1,12 @@
+import { SessionProvider } from 'next-auth/react'
+
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: any) {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  )
+}
