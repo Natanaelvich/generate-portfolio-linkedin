6c892e26fc8156267c091eea48e986943fd70993
natanaelvich <taelima1997@gmail.com>
2023-09-07 17:06:48 -0300

    update with useSession

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
-import ButtonLinkedin from './components/ButtonLinkedin'
+'use client'
+
+import { useSession } from 'next-auth/react'
+import ButtonLinkedin from '../components/ButtonLinkedin'
 
 export default function Home() {
+  const { data: session } = useSession()
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center gap-6 p-24">
       <h1 className="text-4xl font-bold text-center text-gray-900">
@@ -12,7 +17,13 @@ export default function Home() {
       </p>
 
       <div className="flex items-center justify-center mt-4">
-        <ButtonLinkedin />
+        {session?.user?.name ? (
+          <p className="text-center text-gray-700">
+            Welcome {session?.user?.name}
+          </p>
+        ) : (
+          <ButtonLinkedin />
+        )}
       </div>
     </main>
   )
