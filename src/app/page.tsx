edc2f46dc501b1298cba8a1efb7e84ca64551e2e
natanaelvich <taelima1997@gmail.com>
2023-09-08 21:33:05 -0300

    update homepage with logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,19 @@
 
 import { useSession } from 'next-auth/react'
 import ButtonLinkedin from '../components/ButtonLinkedin'
+import Image from 'next/image'
 
 export default function Home() {
   const { data: session } = useSession()
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center gap-6 p-24">
-      <h1 className="text-4xl font-bold text-center text-gray-900">
-        Generate Portfolio Linkedin
-      </h1>
+      <div className="flex items-center justify-center gap-6">
+        <Image src="/portfolio-icon.png" alt="Logo" width={100} height={100} />
+        <h1 className="text-4xl font-bold text-center text-gray-900">
+          Generate Portfolio Linkedin
+        </h1>
+      </div>
 
       <p className="text-center text-gray-700">
         A simple tool to generate a portfolio from your Linkedin
