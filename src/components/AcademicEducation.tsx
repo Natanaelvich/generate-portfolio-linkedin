44e9acd6487ba8de0092ec558ec441b8c2c0a4dc
natanaelvich <taelima1997@gmail.com>
2023-09-08 21:32:29 -0300

    add AcademicEducation component

---
+++ src/components/AcademicEducation.tsx
@@ -0,0 +1,16 @@
+export default function AcademicEducation() {
+  return (
+    <div className="flex flex-col gap-4">
+      <div className="flex flex-col gap-2">
+        <h4>UNIVERSIDADE FEDERAL DO PARANÁ</h4>
+        <h5>Bachelor&apos;s degree in Computer Science</h5>
+        <h6>2016 - 2022</h6>
+        <p>
+          I am currently studying Computer Science at the Federal University of
+          Paraná. I am in the 5th year of the course and I am looking for a job
+          opportunity in the area of software development.
+        </p>
+      </div>
+    </div>
+  )
+}
