78a29645a0d7b209af776fd397c293ce59819516
natanaelvich <taelima1997@gmail.com>
2023-09-08 21:32:18 -0300

    add Expirence component

---
+++ src/components/Expirence.tsx
@@ -0,0 +1,22 @@
+export default function Expirence() {
+  return (
+    <div className="flex flex-col gap-4">
+      <div className="flex flex-col gap-2">
+        <h4>COAMO AGROINDUSTRIAL COOPERATIVA</h4>
+        <h5>Senior Software Engineer ReactJS | React-native | NodeJS</h5>
+        <h6>julho de 2022 - Present (1 ano 3 meses)</h6>
+        <p>
+          Na Coamo, uma grande cooperativa agroindustrial, atuo como líder
+          técnico, focando em soluções de software para sistemas de frete com
+          React.js, React Native e Node.js. Além de implementar soluções
+          eficientes, desempenho um papel importante na criação de testes
+          unitários, de integração e end-to- end, e no treinamento e mentoria de
+          juniores e estagiários. Estou diretamente envolvido com o negócio e os
+          clientes, compreendendo suas necessidades e adaptando as soluções
+          técnicas para melhor atendê-los, garantindo assim o crescimento e
+          sucesso da Coamo.
+        </p>
+      </div>
+    </div>
+  )
+}
