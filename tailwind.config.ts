f700b41e2e4ed17b8f1bbfdddb292bedf71c61c6
natanaelvich <taelima1997@gmail.com>
2023-09-08 21:31:46 -0300

    update with gridTemplateColumns

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,9 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      gridTemplateColumns: {
+        aside: 'minmax(18rem, 20rem) 1fr',
+      },
     },
   },
   plugins: [],
