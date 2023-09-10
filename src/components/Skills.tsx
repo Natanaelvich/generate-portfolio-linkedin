15d6797c68b1e2bc8cba656bb533e239be6b4bc8
natanaelvich <taelima1997@gmail.com>
2023-09-09 21:54:32 -0300

    add Skills component

---
+++ src/components/Skills.tsx
@@ -0,0 +1,21 @@
+export default function Skills() {
+  return (
+    <div>
+      <h2 className="text-2xl font-bold  text-gray-900">Skills</h2>
+      <ul className="flex flex-col gap-2">
+        <li className="bg-gray-200 rounded-full px-4 py-2 text-sm font-semibold text-gray-700">
+          #javascript
+        </li>
+        <li className="bg-gray-200 rounded-full px-4 py-2 text-sm font-semibold text-gray-700">
+          #react
+        </li>
+        <li className="bg-gray-200 rounded-full px-4 py-2 text-sm font-semibold text-gray-700">
+          #nextjs
+        </li>
+        <li className="bg-gray-200 rounded-full px-4 py-2 text-sm font-semibold text-gray-700">
+          #tailwindcss
+        </li>
+      </ul>
+    </div>
+  )
+}
