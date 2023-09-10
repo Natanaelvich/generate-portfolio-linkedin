ff2523aadf691254fda31c7e66e2947e1afad676
natanaelvich <taelima1997@gmail.com>
2023-09-10 10:52:09 -0300

    add AboutComponent

---
+++ src/components/About.tsx
@@ -0,0 +1,13 @@
+export default function About() {
+  return (
+    <div>
+      <h2 className="text-2xl font-bold  text-gray-900">About</h2>
+      <p className=" text-gray-700">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque
+        voluptatum sapiente dicta quidem ratione reprehenderit, soluta ipsam
+        perferendis eum dolor similique repellat eligendi nostrum impedit amet
+        sint nobis sit ea!
+      </p>
+    </div>
+  )
+}
