063e7b5cc408f0f819d51e262b233e1cd617849a
natanaelvich <taelima1997@gmail.com>
2023-09-21 20:25:04 -0300

    update styles AcademicEducation

--- src/components/AcademicEducation.tsx
+++ src/components/AcademicEducation.tsx
@@ -3,9 +3,9 @@ export default function AcademicEducation() {
     <div>
       <h2 className="text-2xl font-bold  text-gray-900">Academic Education</h2>
       <div className="flex flex-col gap-2">
-        <h4>UniFacema</h4>
-        <h5>ANÁLISE E DESENVOLVIMENTO DE SISTEMAS</h5>
-        <h6>(2019 - 2021)</h6>
+        <h4 className="text-gray-700">UniFacema</h4>
+        <h5 className="text-gray-700">ANÁLISE E DESENVOLVIMENTO DE SISTEMAS</h5>
+        <h6 className="text-gray-700">(2019 - 2021)</h6>
       </div>
     </div>
   )
