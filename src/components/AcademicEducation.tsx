37fa1035c2516d9546bc87d9756c16d00c9489f1
natanaelvich <taelima1997@gmail.com>
2023-09-20 22:12:07 -0300

    update academy education example

--- src/components/AcademicEducation.tsx
+++ src/components/AcademicEducation.tsx
@@ -3,14 +3,9 @@ export default function AcademicEducation() {
     <div>
       <h2 className="text-2xl font-bold  text-gray-900">Academic Education</h2>
       <div className="flex flex-col gap-2">
-        <h4>UNIVERSIDADE FEDERAL DO PARANÁ</h4>
-        <h5>Bachelor&apos;s degree in Computer Science</h5>
-        <h6>2016 - 2022</h6>
-        <p>
-          I am currently studying Computer Science at the Federal University of
-          Paraná. I am in the 5th year of the course and I am looking for a job
-          opportunity in the area of software development.
-        </p>
+        <h4>UniFacema</h4>
+        <h5>ANÁLISE E DESENVOLVIMENTO DE SISTEMAS</h5>
+        <h6>(2019 - 2021)</h6>
       </div>
     </div>
   )
