66f4cacf9062598f5905d854ab8d1ae5dedfdac0
natanaelvich <taelima1997@gmail.com>
2023-09-20 22:11:49 -0300

    update about example

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,12 +1,31 @@
 export default function About() {
   return (
     <div>
-      <h2 className="text-2xl font-bold  text-gray-900">About</h2>
-      <p className=" text-gray-700">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque
-        voluptatum sapiente dicta quidem ratione reprehenderit, soluta ipsam
-        perferendis eum dolor similique repellat eligendi nostrum impedit amet
-        sint nobis sit ea!
+      <h2 className="text-5xl font-bold  text-gray-900">About</h2>
+      <p className="text-2xl text-gray-700">
+        Com uma sólida experiência abrangendo várias tecnologias e indústrias,
+        eu desenvolvi um conjunto robusto de habilidades em múltiplas áreas,
+        sempre visando impulsionar a qualidade e eficiência dos projetos que
+        lidero. Na área de mobile, utilizando React Native, participei dos mais
+        diversos tipos de projetos, aprofundando-me em conceitos de offline
+        first, manipulação de mapas e câmeras, bem como trabalhando com
+        localização e lojas tanto Android quanto iOS. No backend, destaco-me no
+        desenvolvimento de APIs REST com Node.js, trabalho com GraphQL,
+        integração entre sistemas e CI/CD. No desenvolvimento web, utilizando
+        React, tenho experiência desde sistemas administrativos até landing
+        pages e dashboards, sempre aplicando testes de unidade, integração e
+        E2E. Adotando sempre as melhores práticas, estou familiarizado com
+        padrões de design como clean code e clean architecture, além de empregar
+        TDD e DDD. Utilizei minhas habilidades em várias áreas, incluindo
+        empregos, imobiliário, agroindústria e rastreamento. Em todas as minhas
+        funções, assumi a liderança técnica, priorizando o treinamento e a
+        mentoria de desenvolvedores juniores e estagiários. Com um forte
+        envolvimento com o negócio e os clientes, tenho uma experiência
+        significativa na definição de requisitos para clientes e coleta de
+        feedback dos usuários. Esse comprometimento com a liderança e com a
+        compreensão das necessidades do cliente aprimorou ainda mais minha
+        capacidade de contribuir de maneira significativa para os projetos e
+        equipes dos quais faço parte.
       </p>
     </div>
   )
