988b1d531449176377752188186af9a74d756323
natanaelvich <taelima1997@gmail.com>
2023-10-04 22:28:32 -0300

    update config singin linkedin

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,36 +1,31 @@
 import NextAuth, { AuthOptions } from 'next-auth'
 import LinkedinProvider from 'next-auth/providers/linkedin'
+
 export const authOptions: AuthOptions = {
-  // Configure one or more authentication providers
   providers: [
     LinkedinProvider({
       clientId: process.env.LINKEDIN_ID as string,
       clientSecret: process.env.LINKEDIN_SECRET as string,
       authorization: {
-        url: 'https://www.linkedin.com/oauth/v2/authorization',
-        params: { scope: 'profile email' },
+        params: {
+          scope: 'openid profile email',
+        },
+      },
+      userinfo: {
+        url: 'https://api.linkedin.com/v2/userinfo',
+      },
+      async profile(profile, tokens) {
+        console.log({
+          profile,
+          tokens,
+        })
+
+        return {
+          ...profile,
+        }
       },
     }),
-
-    // ...add more providers here
   ],
-
-  callbacks: {
-    async signIn(data) {
-      console.log({
-        data,
-      })
-      return true
-    },
-    async session({ session, user, token }) {
-      console.log({
-        session,
-        user,
-        token,
-      })
-      return session
-    },
-  },
 }
 const handler = NextAuth(authOptions)
 
