01e9c7026318d5366a64362a0e6e8f212c027fa2
natanaelvich <taelima1997@gmail.com>
2023-10-15 22:06:44 -0300

    add props to componente Links

--- src/components/Links.tsx
+++ src/components/Links.tsx
@@ -1,44 +1,29 @@
-export default function Links() {
+import { ReactElement } from 'react'
+
+type Link = {
+  href: string
+  label: string
+  icon: ReactElement
+}
+
+type Props = {
+  links: Link[]
+}
+
+export default function Links({ links }: Props) {
   return (
     <div>
       <h2 className="text-2xl font-bold  text-gray-900 mb-6">Links</h2>
       <ul className="flex flex-col gap-6">
-        <li className="flex items-center gap-2">
-          <div className="w-7 h-7">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              x="0px"
-              y="0px"
-              viewBox="0 0 31 31"
-            >
-              <path d="M15,3C8.373,3,3,8.373,3,15c0,5.623,3.872,10.328,9.092,11.63C12.036,26.468,12,26.28,12,26.047v-2.051 c-0.487,0-1.303,0-1.508,0c-0.821,0-1.551-0.353-1.905-1.009c-0.393-0.729-0.461-1.844-1.435-2.526 c-0.289-0.227-0.069-0.486,0.264-0.451c0.615,0.174,1.125,0.596,1.605,1.222c0.478,0.627,0.703,0.769,1.596,0.769 c0.433,0,1.081-0.025,1.691-0.121c0.328-0.833,0.895-1.6,1.588-1.962c-3.996-0.411-5.903-2.399-5.903-5.098 c0-1.162,0.495-2.286,1.336-3.233C9.053,10.647,8.706,8.73,9.435,8c1.798,0,2.885,1.166,3.146,1.481C13.477,9.174,14.461,9,15.495,9 c1.036,0,2.024,0.174,2.922,0.483C18.675,9.17,19.763,8,21.565,8c0.732,0.731,0.381,2.656,0.102,3.594 c0.836,0.945,1.328,2.066,1.328,3.226c0,2.697-1.904,4.684-5.894,5.097C18.199,20.49,19,22.1,19,23.313v2.734 c0,0.104-0.023,0.179-0.035,0.268C23.641,24.676,27,20.236,27,15C27,8.373,21.627,3,15,3z"></path>
-            </svg>
-          </div>
+        {links.map((link) => (
+          <li className="flex items-center gap-2" key={link.href}>
+            <div className="w-7 h-7">{link.icon}</div>
 
-          <p className="text-gray-700">
-            <a href="http://github.com/natanaelvich">github.com/natanaelvich</a>
-          </p>
-        </li>
-        <li className="flex items-center gap-2">
-          <div className="w-7 h-7">
-            <svg viewBox="0 0 128 128">
-              <path
-                fill="#0076b2"
-                d="M116 3H12a8.91 8.91 0 00-9 8.8v104.42a8.91 8.91 0 009 8.78h104a8.93 8.93 0 009-8.81V11.77A8.93 8.93 0 00116 3z"
-              ></path>
-              <path
-                fill="#fff"
-                d="M21.06 48.73h18.11V107H21.06zm9.06-29a10.5 10.5 0 11-10.5 10.49 10.5 10.5 0 0110.5-10.49M50.53 48.73h17.36v8h.24c2.42-4.58 8.32-9.41 17.13-9.41C103.6 47.28 107 59.35 107 75v32H88.89V78.65c0-6.75-.12-15.44-9.41-15.44s-10.87 7.36-10.87 15V107H50.53z"
-              ></path>
-            </svg>
-          </div>
-
-          <p className="text-gray-700">
-            <a href="http://https://www.linkedin.com/in/natanaelvich/">
-              https://www.linkedin.com/in/natanaelvich/
-            </a>
-          </p>
-        </li>
+            <p className="text-gray-700">
+              <a href={link.href}>{link.label}</a>
+            </p>
+          </li>
+        ))}
       </ul>
     </div>
   )
